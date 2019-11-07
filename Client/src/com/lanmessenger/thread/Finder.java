e85c7a50699f052fb094f87048b1d1502a3be101
muzzy121 <mazurpawel@hotmail.com>
2019-11-07 22:46:11 +0100

    New Classes has body now. Should be able to send and get Broadcast packets!

--- Client/src/com/lanmessenger/thread/Finder.java
+++ Client/src/com/lanmessenger/thread/Finder.java
@@ -13,21 +13,22 @@ public class Finder implements Runnable {
     @Override
     public void run() {
         try {
+            InetAddress broadcastAddress = InetAddress.getByName("192.168.88.255");
             DatagramSocket datagramSocket = new DatagramSocket();
             datagramSocket.setBroadcast(true);
 
-            // TODO: 2019-11-07 For test broadcast address set for 255.255.255.255 - need to think how, and if this is possible to send broadcast from more than one interface!!!
+            // TODO: 2019-11-07 For test broadcast address set for 192.168.88.255(wrong idea to send to everyone 255x4) - need to think how, and if this is possible to send broadcast from more than one interface!!!
             byte[] data = "dupa".getBytes();
 
             try {
-                DatagramPacket datagramPacket = new DatagramPacket(data, data.length, InetAddress.getByName("255.255.255.255"), 7778);
+                DatagramPacket datagramPacket = new DatagramPacket(data, data.length, broadcastAddress, 7778);
                 datagramSocket.send(datagramPacket);
                 System.out.println("Buffer sended!");
                 stop();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } catch (SocketException e) {
+        } catch (SocketException | UnknownHostException e) {
             e.printStackTrace();
         }
 
